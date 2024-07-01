caddccb300e6345277c9499663bb8b650e6887f7
ZigaoWang <zigao.wang24@gmail.com>
2024-07-01 22:04:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -131,10 +131,12 @@ def add_custom_style(html_content, css_content=None):
     copy_button_script = """
     <script>
     function copyCode(button) {
-        const code = button.closest('.code-header').nextElementSibling.innerText;
-        navigator.clipboard.writeText(code).then(() => {
+        const codeBlock = button.closest('.code-header').nextElementSibling.querySelector('code');
+        const codeText = codeBlock ? codeBlock.innerText : '';
+    
+        navigator.clipboard.writeText(codeText).then(() => {
             button.innerHTML = '<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check"><path fill-rule="evenodd" d="M13.78 3.22a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06 0l-3.5-3.5a.75.75 0 0 1 1.06-1.06L6 10.44l7.22-7.22a.75.75 0 0 1 1.06 0z"></path></svg>';
-            setTimeout(() => { 
+            setTimeout(() => {
                 button.innerHTML = '<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy js-clipboard-copy-icon"><path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path></svg>';
             }, 2000);
         });
